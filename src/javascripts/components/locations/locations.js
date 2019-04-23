358b0837b6def553beb98b649e07fde5c0415235
Lakia Jones <rachandra_jones@yahoo.com>
2019-04-22 19:28:04 -0500

    WIP

--- src/javascripts/components/locations/locations.js
+++ src/javascripts/components/locations/locations.js
@@ -4,12 +4,33 @@ import './locations.scss';
 
 let locations = [];
 
+const shootTimeClass = (shootTime) => {
+  let selectedClass = '';
+  switch (shootTime) {
+    case 'Morning':
+      selectedClass = 'bg-primary';
+      break;
+    case 'Afternoon':
+      selectedClass = 'bg-secondary';
+      break;
+    case 'Evening':
+      selectedClass = 'bg-success';
+      break;
+    case 'After Dark':
+      selectedClass = 'bg-warning';
+      break;
+    default:
+      selectedClass = '';
+  }
+  return selectedClass;
+};
+
 const domstringBuilder = () => {
   let domstring = '';
   locations.forEach((location) => {
     domstring += `<div id="${location.id}">`;
-    domstring += '<div class="card">';
-    domstring += `<p class="card-header">${location.name}</p>`;
+    domstring += '<div class="card" width=" 10rem">';
+    domstring += `<p class="card-header ${shootTimeClass(location.shootTime)}">${location.name}</p>`;
     domstring += `<img src="${location.imageUrl}"></img>`;
     domstring += `<h5>${location.address}</h5>`;
     domstring += '</div>';
